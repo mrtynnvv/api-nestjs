b7c92450904b502708c919444c3e47e0aeea8b53
mrtynnvv <dm346@mail.ru>
2025-10-15 10:40:22 +0300

    edit database url

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,11 +10,11 @@ module.exports = {
       max_memory_restart: '512M',
       env: {
         NODE_ENV: 'development',
-        DATABASE_URL: 'file:../../external-db/dev.sqlite',
+        DATABASE_URL: 'file:../../external-db/app.sqlite',
       },
       env_production: {
         NODE_ENV: 'production',
-        DATABASE_URL: 'file:../../external-db/prod.sqlite',
+        DATABASE_URL: 'file:../../external-db/app.sqlite',
       },
     },
   ],
