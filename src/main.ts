b299578da213280f05c68ee15ed4c6695a198f69
mrtynnvv <dm346@mail.ru>
2025-10-21 10:06:49 +0300

    cors upd

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,18 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      'https://mrtynnvv.ru',
+      'https://calorielly.ru',
+    ],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorisation'],
+    credentials: true,
+    maxAge: 86400,
+  });
   await app.listen(3000);
 }
 bootstrap();
