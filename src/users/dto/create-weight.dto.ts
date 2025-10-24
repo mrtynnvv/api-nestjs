82259b7118602715a5765f157aa88f7b6719066e
mrtynnvv <dm346@mail.ru>
2025-10-24 16:16:10 +0300

    maxDecimalPlaces fix

--- src/users/dto/create-weight.dto.ts
+++ src/users/dto/create-weight.dto.ts
@@ -2,7 +2,7 @@ import { IsNumber, Min, Max } from 'class-validator';
 import { Type } from 'class-transformer';
 export class CreateWeightDto {
   @Type(() => Number)
-  @IsNumber({ maxDecimalPlaces: 1 })
+  @IsNumber({ maxDecimalPlaces: 2 })
   @Min(30)
   @Max(500)
   weight: number;
